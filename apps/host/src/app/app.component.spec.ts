6a79e218ad94479b96af63189b974e267254996b
joalopezmo <joalopezmo@uoc.edu>
2024-02-14 01:28:02 +0100

    Add animations module to app.component.spec.ts

--- apps/host/src/app/app.component.spec.ts
+++ apps/host/src/app/app.component.spec.ts
@@ -40,6 +40,7 @@
   //   expect(compiled.querySelector('h1')?.textContent).toContain('Welcome host');
   // }));
   import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
   
@@ -49,7 +50,7 @@ import { AppComponent } from './app.component';
   
     beforeEach(async () => {
       await TestBed.configureTestingModule({
-        imports: [RouterTestingModule],
+        imports: [RouterTestingModule, provideAnimations()],
       }).compileComponents();
     });
   
