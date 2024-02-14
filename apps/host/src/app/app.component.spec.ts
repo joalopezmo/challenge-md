af3752b01aa3a64f312e215c5f806974753244f2
joalopezmo <joalopezmo@uoc.edu>
2024-02-14 01:32:55 +0100

    Refactor app.component.spec.ts

--- apps/host/src/app/app.component.spec.ts
+++ apps/host/src/app/app.component.spec.ts
@@ -1,34 +1,37 @@
-// import { TestBed } from '@angular/core/testing';
-// import { RouterTestingModule } from '@angular/router/testing';
-// import { SharedMaterialUiModule } from '@challenge-md/ui';
-// import { AppComponent } from './app.component';
-// import { NxWelcomeComponent } from './nx-welcome.component';
+import { TestBed } from '@angular/core/testing';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { SharedMaterialUiModule } from '@challenge-md/ui';
+import { AppComponent } from './app.component';
+import { NxWelcomeComponent } from './nx-welcome.component';
 
-// describe('AppComponent', () => {
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       imports: [
-//         SharedMaterialUiModule,
-//         RouterTestingModule.withRoutes([
-//           { path: '', component: NxWelcomeComponent },
-//         ]),
-//         AppComponent,
-//         NxWelcomeComponent,
-//       ],
-//     }).compileComponents();
-//   });
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        SharedMaterialUiModule,
+        RouterTestingModule.withRoutes([
+          { path: '', component: NxWelcomeComponent },
+        ]),
+        AppComponent,
+        NxWelcomeComponent,
+        
+      ],
+    }).compileComponents();
+    provideAnimations();
+  });
 
-  // it('should create the app', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app).toBeTruthy();
-  // });
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
 
-  // it(`should have as title 'host'`, () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app.title).toEqual('host');
-  // });
+  it(`should have as title 'host'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('host');
+  }); }
 
   // it('should render title', fakeAsync(() => {
   //   const fixture = TestBed.createComponent(AppComponent);
@@ -39,34 +42,4 @@
   //   const compiled = fixture.nativeElement as HTMLElement;
   //   expect(compiled.querySelector('h1')?.textContent).toContain('Welcome host');
   // }));
-//   import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { provideAnimations } from '@angular/platform-browser/animations';
-// import { RouterTestingModule } from '@angular/router/testing';
-// import { AppComponent } from './app.component';
-  
-//   describe('AppComponent', () => {
-//     let component: AppComponent;
-//     let fixture: ComponentFixture<AppComponent>;
-  
-//     beforeEach(async () => {
-//       await TestBed.configureTestingModule({
-//         imports: [RouterTestingModule, provideAnimations()],
-//       }).compileComponents();
-//     });
-  
-//     beforeEach(() => {
-//       fixture = TestBed.createComponent(AppComponent);
-//       component = fixture.componentInstance;
-//       fixture.detectChanges();
-//     });
-  
-//     it('should create the app', () => {
-//       expect(component).toBeTruthy();
-//     });
-  
-//     it(`should have as title 'host'`, () => {
-//       expect(component.title).toEqual('host');
-//     });
-  
-//   });
-  
+);
