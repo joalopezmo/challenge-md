6973b0350f63c2401861f7d90808ce21bb80d24f
joalopezmo <joalopezmo@uoc.edu>
2024-02-14 01:22:38 +0100

    Refactor unit tests for AppComponent

--- apps/host/src/app/app.component.spec.ts
+++ apps/host/src/app/app.component.spec.ts
@@ -1,28 +1,28 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { SharedMaterialUiModule } from '@challenge-md/ui';
-import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+// import { TestBed } from '@angular/core/testing';
+// import { RouterTestingModule } from '@angular/router/testing';
+// import { SharedMaterialUiModule } from '@challenge-md/ui';
+// import { AppComponent } from './app.component';
+// import { NxWelcomeComponent } from './nx-welcome.component';
 
-describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [
-        SharedMaterialUiModule,
-        RouterTestingModule.withRoutes([
-          { path: '', component: NxWelcomeComponent },
-        ]),
-        AppComponent,
-        NxWelcomeComponent,
-      ],
-    }).compileComponents();
-  });
+// describe('AppComponent', () => {
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [
+//         SharedMaterialUiModule,
+//         RouterTestingModule.withRoutes([
+//           { path: '', component: NxWelcomeComponent },
+//         ]),
+//         AppComponent,
+//         NxWelcomeComponent,
+//       ],
+//     }).compileComponents();
+//   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
+  // it('should create the app', () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.componentInstance;
+  //   expect(app).toBeTruthy();
+  // });
 
   // it(`should have as title 'host'`, () => {
   //   const fixture = TestBed.createComponent(AppComponent);
@@ -39,4 +39,38 @@ describe('AppComponent', () => {
   //   const compiled = fixture.nativeElement as HTMLElement;
   //   expect(compiled.querySelector('h1')?.textContent).toContain('Welcome host');
   // }));
-});
+  import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+  
+  describe('AppComponent', () => {
+    let component: AppComponent;
+    let fixture: ComponentFixture<AppComponent>;
+  
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        declarations: [AppComponent],
+        imports: [RouterTestingModule],
+      }).compileComponents();
+    });
+  
+    beforeEach(() => {
+      fixture = TestBed.createComponent(AppComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    });
+  
+    it('should create the app', () => {
+      expect(component).toBeTruthy();
+    });
+  
+    it(`should have as title 'host'`, () => {
+      expect(component.title).toEqual('host');
+    });
+  
+    it('should render title in a h1 tag', () => {
+      const compiled = fixture.nativeElement;
+      expect(compiled.querySelector('h1').textContent).toContain('host');
+    });
+  });
+  
